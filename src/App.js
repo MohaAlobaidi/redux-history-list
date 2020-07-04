47084abac9f815fc7925de6384e00a8464c595f1
MOHAMMED <mohammed@MOHAMMEDs-MacBook-Pro.local>
2020-07-04 23:10:29 +0200

    list history

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux'
 
-function App() {
+class App extends Component {
+ render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='age'> age : <span> {this.props.age}</span></div>
+      <button className='ageUp' onClick={this.props.onAgeUp}>age Up</button>
+      <button className='ageDown' onClick={this.props.onAgeDOWN}>age Down</button>
+      <div>History</div>
+      <div>
+        <ul>
+          {
+            this.props.history.map(el => (
+              <li className='historyItems' key={el.id}  onClick={()=> this.props.onDelItem(el.id)}>
+                {el.age}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
+ }
 }
 
-export default App;
+const mapStateToProps = (state)=>{
+  return{
+    age:state.age,
+    history:state.history
+  }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+  return{
+    onAgeUp : ()=>(dispatch({type:'AGE_UP',value:1})),
+    onAgeDOWN: ()=> (dispatch({type:'AGE_DOWN',value:1})),
+    onDelItem: (id)=> (dispatch({type:'DEL_ITEM',key: id}))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (App);
